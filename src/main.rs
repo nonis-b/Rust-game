c80b50e24204de348792803401f3b8a3624c21ca
nonis-b <nonis.b@gmail.com>
2021-12-17 21:14:40 +0100

    add sdl image

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
+use sdl2::image::{InitFlag, LoadTexture};
  
 pub fn main() {
+    let png = "res/can.png";
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG);
  
     let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
         .position_centered()
@@ -15,6 +18,8 @@ pub fn main() {
         .unwrap();
  
     let mut canvas = window.into_canvas().build().unwrap();
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture(png).unwrap();
  
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
@@ -25,6 +30,8 @@ pub fn main() {
         i = (i + 1) % 255;
         canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
         canvas.clear();
+        canvas.copy(&texture, None, None).unwrap();
+        canvas.present();
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
